408cc236cd56fdb9ee09e9f8bd668cb7388bf7bd
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-02-06 15:44:01 +0600

    Started working on frontend

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        App
+    <div class="flex flex-column flex-1 h-screen overflow-y-hidden">
+        <Nav />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import Nav from './Nav';
+
     export default {
-        name: "App"
+        name: "App",
+
+        components: {
+            Nav
+        }
     }
 </script>
